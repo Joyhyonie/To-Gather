553300521e5854886f30d34797f3c71bf74cbfea
Joyhyonie <cho970129@gmail.com>
2023-03-17 12:37:18 +0900

    ✨ Feat: 메인페이지 이미지 슬라이드, 프로젝트 상세 페이지 전환 기능 구현

--- src/main/resources/static/user/main/js/main.js
+++ src/main/resources/static/user/main/js/main.js
@@ -19,4 +19,67 @@ window.onload = function() {
             }
         }
     })
+
+    /* 메인 배너 슬라이드 */
+    const slides = document.querySelectorAll('.slide');
+    const slider = document.querySelector('.slider');
+    const prevBtn = document.querySelector('.prev');
+    const nextBtn = document.querySelector('.next');
+      
+    let currentSlide = 0;
+    let slideInterval;
+      
+    function showSlide(n) {
+        if (n < 0) {
+            currentSlide = slides.length - 1;
+        } else if (n >= slides.length) {
+            currentSlide = 0;
+        }
+        for (let i = 0; i < slides.length; i++) {
+            if (i < currentSlide) {
+                slides[i].classList.add('slide-left');
+                slides[i].classList.remove('slide-right', 'current-slide');
+            } else if (i === currentSlide) {
+                slides[i].classList.add('current-slide');
+                slides[i].classList.remove('slide-right', 'slide-left');
+            } else {
+                slides[i].classList.add('slide-right');
+                slides[i].classList.remove('slide-left', 'current-slide');
+            }
+        }
+    }
+      
+    function prevSlide() {
+        currentSlide--;
+        showSlide(currentSlide);
+    }
+      
+    function nextSlide() {
+        currentSlide++;
+        showSlide(currentSlide);
+    }
+
+    prevBtn.addEventListener('click', () => {
+        clearInterval(slideInterval);
+        prevSlide();
+    });
+      
+    nextBtn.addEventListener('click', () => {
+        clearInterval(slideInterval);
+        nextSlide();
+    });
+      
+    slider.addEventListener('mouseover', () => {
+        clearInterval(slideInterval);
+    });
+      
+    slider.addEventListener('mouseout', () => {
+        slideInterval = setInterval(() => {
+          nextSlide();
+        }, 2500);
+    });
+      
+    slideInterval = setInterval(() => {
+        nextSlide();
+    }, 2500);
 }
--- src/main/resources/static/user/project/js/viewProjectDetail.js
+++ src/main/resources/static/user/project/js/viewProjectDetail.js
@@ -1,2 +1,27 @@
+window.onload = function() {
 
+    /* 메뉴 탭 클릭 시 스토리 or 소식&후기 화면 전환 */
+    const menuList = document.querySelectorAll('.story-bar .menu li');
+    const contents = document.querySelectorAll('.story-body .content');
+    let activeCont = ''; // 현재 활성화된 컨텐츠 (기본으로 스토리 활성화)
+
+    for(let i = 0; i < menuList.length; i++) {
+        menuList[i].querySelector('.btn').addEventListener('click', function(e){
+            e.preventDefault();
+                    
+             for(let j = 0; j < menuList.length; j++) {
+                menuList[j].classList.remove('is_on'); /* 나머지 버튼 클래스 제거 */
+
+                contents[j].style.display = 'none'; /* 나머지 컨텐츠 display:none 처리 */
+            }
+
+            /* 버튼 관련 이벤트 */
+            this.parentNode.classList.add('is_on');
+
+            /* 버튼 클릭 시 컨텐츠 전환 */
+            activeCont = this.getAttribute('href');
+            document.querySelector(activeCont).style.display = 'block';
+        });
+    }
+}
 
