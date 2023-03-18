8240d5fd1d63ed0ba5d5bc2e5c155afa54d27a4b
Joyhyonie <cho970129@gmail.com>
2023-03-18 22:24:16 +0900

    💄 Design: 타임리프 사용 후 css 수정

---
+++ src/main/java/com/greedy/togather/user/project/controller/ProjectController.java
@@ -0,0 +1,15 @@
+package com.greedy.togather.user.project.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+@RequestMapping("/user/project")
+public class ProjectController {
+
+
+}
