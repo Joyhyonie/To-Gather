57e12ca21caea0ab07fb3500f72d1b0c2c0d61d5
Joyhyonie <cho970129@gmail.com>
2023-03-20 22:57:15 +0900

    ✅ Test: 프로젝트 리스트 및 상세 페이지 매퍼 테스트 완료

--- src/main/java/com/greedy/togather/user/project/dto/ReplyDTO.java
+++ src/main/java/com/greedy/togather/user/project/dto/ReplyDTO.java
@@ -12,6 +12,7 @@ public class ReplyDTO {
 	private String replyBody;	// 댓글 내용
 	private Date replyRegDate;	// 작성일
 	private int donation;		// 기부금
+	private String userId;		// SUBSTR(B.EMAIL, 1, INSTR(B.EMAIL, '@') -1) 회원 아이디
 	
 	private ReplyWriterDTO writer;	// 댓글 작성자 정보
 }
