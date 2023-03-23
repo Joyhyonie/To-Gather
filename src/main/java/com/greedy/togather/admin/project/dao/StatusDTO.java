7e3838b30c65b3f61ff5b32fc9eae7cf82360d56
YoungJu <kyj198706@gmail.com>
2023-03-23 21:43:13 +0900

    💄 Design: admin project, mainbanner, coupon 업데이트

---
+++ src/main/java/com/greedy/togather/admin/project/dao/StatusDTO.java
@@ -0,0 +1,13 @@
+package com.greedy.togather.admin.project.dao;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class StatusDTO {
+	
+	private String searchCondition;
+	private String searchValue;
+
+}
---
+++ src/main/java/com/greedy/togather/admin/project/dto/StatusDTO.java
@@ -0,0 +1,11 @@
+package com.greedy.togather.admin.project.dto;
+
+import lombok.Data;
+
+@Data
+public class StatusDTO {
+	
+	private String searchCondition;
+	private String searchValue;
+
+}
