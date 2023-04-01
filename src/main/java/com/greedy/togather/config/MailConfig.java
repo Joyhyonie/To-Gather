4aea95754d1d099a578c06dec20ff6e7db3e5858
Joyhyonie <cho970129@gmail.com>
2023-04-01 23:53:46 +0900

    🎉 Init: 프로젝트 보완 시작

--- src/main/java/com/greedy/togather/config/MailConfig.java
+++ src/main/java/com/greedy/togather/config/MailConfig.java
@@ -15,7 +15,7 @@ public class MailConfig {
         JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
 
         javaMailSender.setHost("smtp.naver.com"); // 메인 도메인 서버 주소 => 정확히는 smtp 서버 주소
-        javaMailSender.setUsername("rth0204@naver.com"); // 네이버 아이디
+        javaMailSender.setUsername(""); // 네이버 아이디
         javaMailSender.setPassword(""); // 네이버 비밀번호
 
         javaMailSender.setPort(465); // 메일 인증서버 포트
